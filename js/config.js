8b35b6adbfd136fd044685640e23fcb0e35d565f
omimo-bit <dwicky@araiagroup.com>
2026-09-16 11:36:53 +0700

    Delete js directory

--- js/config.js
+++
@@ -1,5 +0,0 @@
-window.APP_CONFIG={
-  API_URL:'https://script.google.com/macros/s/AKfycbyM7bg_jSg4b0sGD6383trqYY-PeWgKTqTsavDXqBhYyzLFXOoYjU1bf175wYXHMTQOuQ/exec',
-  APP_NAME:'DOTSIXTWOO Project Control Center',
-  TOKEN_KEY:'d62_session_token'
-};
