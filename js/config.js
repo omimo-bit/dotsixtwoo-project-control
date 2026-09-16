429c3e6d6c19b2014f7c84dcd631c16e6de8b935
omimo-bit <dwicky@araiagroup.com>
2026-09-16 19:50:51 +0700

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,5 @@
+window.APP_CONFIG={
+  API_URL:'https://script.google.com/macros/s/AKfycbz-SyI406R8oxzfZ05D3M9H9VNNn0O6u6sKADG-VIpL6J-ZJ5nT-KYEeGx-SAvo_Yf6ag/exec',
+  APP_NAME:'DOTSIXTWOO Project Control Center',
+  TOKEN_KEY:'d62_session_token'
+};
