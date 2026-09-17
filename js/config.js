8cd1a6bdf71823d33c80b2f10c15031bac462c6c
omimo-bit <dwicky@araiagroup.com>
2026-09-17 16:38:30 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG={
-  API_URL:'https://script.google.com/macros/s/AKfycbz-SyI406R8oxzfZ05D3M9H9VNNn0O6u6sKADG-VIpL6J-ZJ5nT-KYEeGx-SAvo_Yf6ag/exec',
+  API_URL:'https://script.google.com/macros/s/AKfycbxxrYDBKEFTP5zsyCm3W7G-W8po3MFF6Fd4kwMx8F-r9hR-wwopVARehwcXHExmqGyk9w/exec',
   APP_NAME:'DOTSIXTWOO Project Control Center',
   TOKEN_KEY:'d62_session_token'
 };
