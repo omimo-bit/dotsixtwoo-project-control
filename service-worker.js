ba08b6b65ae8adee2123e6c2400e13fd745268e4
omimo-bit <dwicky@araiagroup.com>
2026-09-24 00:25:41 +0700

    Update service worker cache and add notification click
    Updated cache version and added notification click handler.

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,7 @@
-const CACHE='dotsixtwoo-v23-20260916';
+const CACHE='dotsixtwoo-v24-20260923';
 const ASSETS=['./','./index.html','./css/style.css','./js/config.js','./js/api.js','./js/app.js','./manifest.json','./assets/logo.png','./assets/icons/icon-192.png','./assets/icons/icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
+
+self.addEventListener('notificationclick',e=>{e.notification.close();e.waitUntil(clients.matchAll({type:'window',includeUncontrolled:true}).then(list=>{for(const c of list){if('focus' in c)return c.focus()}return clients.openWindow?clients.openWindow('./'):null}))});
